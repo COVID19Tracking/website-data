d9e7ffef740dc6d0b79c985533c399f168615904
Kevin Miller <keveemiller@gmail.com>
2020-09-24 11:35:08 -0700

    chore: Add LTC algolia index update

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ const commandLineArgs = require('command-line-args')
 const volunteers = require('./lib/volunteers')
 const screenshots = require('./lib/screenshots')
 const bigQuery = require('./lib/bigquery')
+const longTermCareFacilities = require('./lib/long-term-care-facilities')
 
 const options = commandLineArgs([
   { name: 'volunteers', alias: 'v', type: Boolean },
   { name: 'screenshots', alias: 's', type: Boolean },
   { name: 'bigquery', alias: 'b', type: Boolean },
+  { name: 'ltcfacilities', alias: 'l', type: Boolean },
 ])
 
 const tasks = []
@@ -21,6 +23,10 @@ if (options.screenshots) {
 
 if (options.bigquery) {
   tasks.push(bigQuery())
+}
+
+if (options.ltcfacilities) {
+  tasks.push(longTermCareFacilities())
 }
 
 Promise.all(tasks).then(() => {
