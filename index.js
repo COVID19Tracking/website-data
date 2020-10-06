4092988a80dbac596d282560a43ea55e0ea9a970
Kevin Miller <keveemiller@gmail.com>
2020-10-06 16:28:48 -0700

    chore: Add twitter build

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const volunteers = require('./lib/volunteers')
 const screenshots = require('./lib/screenshots')
 const bigQuery = require('./lib/bigquery')
 const longTermCareFacilities = require('./lib/long-term-care-facilities')
+const tweets = require('./lib/tweets')
 
 const options = commandLineArgs([
   { name: 'volunteers', alias: 'v', type: Boolean },
   { name: 'screenshots', alias: 's', type: Boolean },
   { name: 'bigquery', alias: 'b', type: Boolean },
   { name: 'ltcfacilities', alias: 'l', type: Boolean },
+  { name: 'tweets', alias: 't', type: Boolean },
 ])
 
 const tasks = []
@@ -27,6 +29,10 @@ if (options.bigquery) {
 
 if (options.ltcfacilities) {
   tasks.push(longTermCareFacilities())
+}
+
+if (options.tweets) {
+  tasks.push(tweets())
 }
 
 Promise.all(tasks).then(() => {
